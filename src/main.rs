65cc45754e23fbbdf57d953c0ee1c43432aac7dc
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-31 21:54:41 -0700

    Add Game struct that holds players, add Play struct that holds the card being played, if it is a lie, and the target. Add random hand generation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use rand::prelude::*;
 use std::fmt;
+
+struct Game {
+    players: Vec<Player>,
+}
 
 struct Player {
     name: String,
     coins: u8,
     cards: Vec<Card>,
+    current_play: Option<Play>,
+}
+
+struct Play {
+    card: Card,
+    is_lie: bool,
+    target: Box<Player>,
 }
 
 struct Card {
@@ -11,8 +23,6 @@ struct Card {
     action: Action,
     effect: Effect,
     counteraction: Counteraction,
-    target: Option<Player>,
-    is_lie: bool,
 }
 
 enum Character {
@@ -90,7 +100,7 @@ impl fmt::Display for Effect {
         let effect: String = match *self {
             Effect::X => String::from("X"),
             Effect::Take(coins) => format!("Take {coins} coins"),
-            Effect::Kill(coins) => format!("Pay {coins} coins.\n Choose a player to loose influence"),
+            Effect::Kill(coins) => format!("Pay {coins} coins.\nChoose a player to loose influence"),
             Effect::ExchangeCards => String::from("Exchange cards with Court Deck"),
             Effect::TakeTwoFromOtherPlayer => String::from("Take 2 coins from another player")
         };
@@ -126,20 +136,87 @@ impl fmt::Display for Card {
 }
 
 impl Card {
+    fn random() -> Card {
+        let n = thread_rng().gen_range(0..=5);
+
+        match n {
+            0 => Card::duke(),
+            1 => Card::assassin(),
+            3 => Card::ambassador(),
+            4 => Card::captain(),
+            5 => Card::contessa(),
+
+            _ => Card::contessa(),
+        }
+    }
+
+    fn full_draw() -> Vec<Card> {
+        vec![Card::random(), Card::random()]
+    }
+
     fn duke() -> Card {
         Card { 
             character: Character::Duke, 
             action: Action::Tax, 
             effect: Effect::Take(3), 
             counteraction: Counteraction::BlocksForeignAid, 
-            target: None, 
-            is_lie: false 
+        }
+    }
+    fn assassin() -> Card {
+        Card { 
+            character: Character::Assassin, 
+            action: Action::Assassinate, 
+            effect: Effect::Kill(3), 
+            counteraction: Counteraction::X, 
+        }
+    }
+    fn ambassador() -> Card {
+        Card { 
+            character: Character::Ambassador, 
+            action: Action::Exchange, 
+            effect: Effect::ExchangeCards, 
+            counteraction: Counteraction::BlocksStealing, 
+        }
+    }
+    fn captain() -> Card {
+        Card { 
+            character: Character::Captain, 
+            action: Action::Steal, 
+            effect: Effect::TakeTwoFromOtherPlayer, 
+            counteraction: Counteraction::BlocksStealing, 
+        }
+    }
+    fn contessa() -> Card {
+        Card { 
+            character: Character::Contessa, 
+            action: Action::X, 
+            effect: Effect::X, 
+            counteraction: Counteraction::BlocksAssasination, 
         }
     }
 }
 
-fn main() {
-    let duke = Card::duke();
+impl Game {
+    fn new() -> Game {
+        Game { 
+            players: Vec::new()
+        }
+    }
 
-    println!("{}", duke);
+    fn add_player(&mut self, name: String, cards: Vec<Card>) {
+        self.players.push(Player {
+            name,
+            coins: 0,
+            cards,
+            current_play: None,
+        });
+    }
+}
+
+fn main() {
+    let mut game = Game::new();
+
+    game.add_player(String::from("Nate"), Card::full_draw());
+
+    println!("{}\n\n{}", game.players[0].cards[0], game.players[0].cards[1]);
 }
